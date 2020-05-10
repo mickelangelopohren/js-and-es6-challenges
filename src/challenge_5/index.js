5597ab1fed4278f6be9cc3b565310c175c152d16
Mickelangelo Pohren <mickelangelo.ponhren@gmail.com>
2020-05-10 18:53:58 -0300

    Challenges 6 and 7 solution

--- src/challenge_5/index.js
+++ src/challenge_5/index.js
@@ -4,20 +4,20 @@ Change contents of the isNumber function
 using ternary operator.
 */
 
-"use strict";
 
 function isNumber(a) {
-  if (typeof a === "number") {
-    return "That's number";
-  } else {
-    return "That's not a number";
-  }
+  // if (typeof a === "number") {
+  //   return "That's number";
+  // } else {
+  //   return "That's not a number";
+  // }
+  return typeof a === 'number' ? 'That\'s number' : 'That\'s not a number';
 }
 
 console.log(isNumber(10));
 // That's number
 
-console.log(isNumber("Hey there"));
+console.log(isNumber('Hey there'));
 // That's not a number
 
 console.log(isNumber(true));
---
+++ src/challenge_6/index.js
@@ -0,0 +1,17 @@
+/* CHALLENGE 6 - Arrow Functions
+
+Use arrow functions instead of functions
+where possible.
+*/
+
+
+// function mult(a, b) {
+//   return a * b;
+// }
+const mult = (a, b) => a * b;
+
+// setTimeout(function() {
+setTimeout(() => {
+  console.log(mult(5, 10));
+}, 1000);
+// 50
